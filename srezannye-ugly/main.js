5a766faf8ef0eff238aa126aaa6c35206f4258bb
BOBKOVSERGEY <sergey_bobkov@inbox.ru>
2017-11-24 15:25:15 +0300

    Скошенные углы

--- srezannye-ugly/main.js
+++ srezannye-ugly/main.js
@@ -7,7 +7,7 @@ $(document).ready(function(){
   if(!animateHeaderTitle){
     // Drop in and rotate the logo
     $('#headerTitle').css({ top: '-300px' });
-    $('#headerTitle').animate({ top: '0px' }, { easing: 'easeOutElastic', duration: 3000 });
+    $('#headerTitle').animate({ top: '0px' }, { easing: 'easeOutElastic', duration: 5000 });
   }
 
   // Fade in the content area (doesn't work in IE thanks to the PNG opacity bug)
