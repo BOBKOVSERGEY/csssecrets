e14e39798fd683b1b2dfdabe2aa5827f27d40364
BOBKOVSERGEY <sergey_bobkov@inbox.ru>
2017-12-14 12:24:00 +0300

    diagram svg

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 var bs = require("browser-sync").create();
 
-bs.watch(['srezannye-ugly/*.*', 'srezannye-ugly/css/*.*']).on("change", bs.reload);
+bs.watch(['diagram/*.*', 'diagram/css/*.*']).on("change", bs.reload);
 
 // Start a Browsersync proxy
 bs.init({
-  server: "./srezannye-ugly"
+  server: "./diagram"
 });
