45d1855b8495b82c680b7598239dabb4d58e33f2
BOBKOVSERGEY <sergey_bobkov@inbox.ru>
2017-12-18 11:13:42 +0300

    Filters

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 var bs = require("browser-sync").create();
 
-bs.watch(['diagram/*.*', 'diagram/css/*.*']).on("change", bs.reload);
+bs.watch(['filters/*.*', 'filters/css/*.*']).on("change", bs.reload);
 
 // Start a Browsersync proxy
 bs.init({
-  server: "./diagram"
+  server: "./filters"
 });
