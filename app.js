fb02a7e2ea05275260c60b92eaf77eb9e6545dd5
SERGEYBOBKOV <sergey_bobkov@inbox.ru>
2018-04-27 14:12:39 +0300

    text по кругу

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 var bs = require("browser-sync").create();
 
-bs.watch(['effect-motirovannogo-stekla/*.*', 'effect-motirovannogo-stekla/css/*.*']).on("change", bs.reload);
+bs.watch(['text/*.*', 'text/css/*.*']).on("change", bs.reload);
 
 // Start a Browsersync proxy
 bs.init({
-  server: "./effect-motirovannogo-stekla"
+  server: "./text"
 });
